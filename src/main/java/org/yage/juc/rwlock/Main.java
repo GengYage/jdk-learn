c150d1c2ca6a4d08db846b396515485f5c9f0d0e
Yage <gengyage@xylink.com>
2023-03-28 20:02:41 +0800

    rw lock case

---
+++ src/main/java/org/yage/juc/rwlock/Main.java
@@ -0,0 +1,13 @@
+package org.yage.juc.rwlock;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author: Yage
+ * @create: 2023-03-28 19:44
+ */
+public class Main {
+    public static void main(String[] args) {
+        ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+    }
+}
